033c01706802075b01c7ac227ba8b8776bc8599e
Chase Benedict <chasebenedict1@gmail.com>
2019-02-09 02:26:36 -0600

    Add files via upload

---
+++ Number.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Number : MonoBehaviour
+{
+    public static int numberValue = 0;
+    Text numberText;
+
+    void Start()
+    {
+        numberText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        numberText.text = "Question number: " + numberValue;
+    }
+}
