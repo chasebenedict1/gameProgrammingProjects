fdbeab70b6a85d58bb9dc5ba49967d79e027c5fe
Chase Benedict <chasebenedict1@gmail.com>
2019-02-23 03:08:42 -0600

    Add files via upload

---
+++ Quiz Game/Percent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Percent : MonoBehaviour
+{
+    public static int percentValue = 0;
+    Text percentText;
+
+    void Start()
+    {
+        percentText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        percentText.text = "Percent correct: " + percentValue + "%";
+    }
+}
